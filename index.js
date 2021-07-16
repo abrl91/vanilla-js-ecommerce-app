baa3b89ca7f8da85dcb91359aa25b8bf6bb3e3b6
Amit Barletz <amitbarletz@outlook.com>
2021-07-16 17:55:53 +0300

    products

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express         = require('express');
 const bodyParser      = require('body-parser');
 const cookieSession   = require('cookie-session');
 const authRouter      = require('./routes/admin/auth');
+const productRouter   = require('./routes/admin/products');
 const app             = express();
 
 app.use(express.static('public'));
@@ -11,6 +12,7 @@ app.use(cookieSession({
 }));
 
 app.use(authRouter);
+app.use(productRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
