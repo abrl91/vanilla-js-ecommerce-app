976c320e58205f6391e6cce2b6ff3de971ffb236
Amit Barletz <amitbarletz@outlook.com>
2021-07-24 17:30:41 +0300

    add to cart

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cookieSession         = require('cookie-session');
 const authRouter            = require('./routes/admin/auth');
 const adminProductRouter    = require('./routes/admin/products');
 const productsRouter        = require('./routes/products/products');
+const cartRouter            = require('./routes/cart/carts');
 const app                   = express();
+
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,9 +14,11 @@ app.use(cookieSession({
   keys: ['a1b2c3d4']
 }));
 
+
 app.use(authRouter);
 app.use(adminProductRouter);
 app.use(productsRouter);
+app.use(cartRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
